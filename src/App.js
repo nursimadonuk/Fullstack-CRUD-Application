9fe5e7a51aecd6be1c7a5cbaa2cdd8f2080352a6
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-13 11:10:12 -0500

    added routes for viewing collections and individuals

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const Home = () => (<h1>Home</h1>) ;
+  const Campuses = () => (<h1>Campuses</h1>) ;
+  const Students = () => (<h1>Students</h1>) ;
+  const ShowID = (props) => {
+    console.log(props);
+    return (<div><h1>Path: {props.location.pathname} </h1> <h1>ID: {props.match.params.id}</h1></div>);
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path="/" render={Home} />
+        <Route exact path="/students" render={Students} />
+        <Route exact path="/campuses" render={Campuses} />
+        <Route path="/student/:id/view" render={ShowID} />
+        <Route path="/student/:id/edit" render={ShowID} />
+        <Route path="/campus/:id/view" render={ShowID} />
+        <Route path="/campus/:id/edit" render={ShowID} />
+      </Router>
     </div>
   );
 }
