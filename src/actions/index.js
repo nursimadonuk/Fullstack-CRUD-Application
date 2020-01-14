ade38c7379413b06b9c2e3e5406836ebe1c582db
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-14 11:30:37 -0500

    defined data structure

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,24 @@
 import { ADD_CAMPUS, ADD_STUDENT } from "../constants/action-types";
 
+/* Payload should be a Campus object with these properties:
+	name: string,
+	id: int,
+	address: string,
+	description: string,
+	location: string,
+	image: url
+ */
 export function addCampus(payload) {
   return { type: ADD_CAMPUS, payload };
 }
 
+/* Paylod should be a Student object with these properties:
+	firstName: string,
+	lastName: string,
+	email: string,
+	image: url,
+	gpa: float
+*/
 export function addStudent(payload) {
   return { type: ADD_STUDENT, payload };
 }
