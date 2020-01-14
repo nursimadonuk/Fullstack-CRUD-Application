bec151c9b4442d358c8aaf99031f617e30780c73
Xing Tao Shi <xshi@stuy.edu>
2020-01-14 10:55:53 -0500

    worked on student view

--- src/components/containers/StudentContainer.jsx
+++ src/components/containers/StudentContainer.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 import "./StudentContainer.css";
 import NoAvatarImage from "./../../images/no_avatar.png";
 
 export default class StudentContainer extends Component {
   delete = () => {
-    console.log("Deleting");
+    console.log(this.props.student);
   };
 
   render() {
+    let viewURL = "/student/" + this.props.student.id + "/view/";
     return (
       <div className="boxed">
         <div className="column">
-          <img alt="" src={NoAvatarImage} />
+          <img src={this.props.student.image} alt="" />
         </div>
+        <div className="column">Student Name: {this.props.student.name}</div>
         <div className="column">
-          Student Name: Lorem Ipsum <br /> Campus Name: Lorem Ipsum
-        </div>
-        <div className="column">
-          <Link style={{ color: "purple" }} to="/campuses">
+          <Link style={{ color: "purple" }} to={viewURL}>
             View
           </Link>
           <br />
@@ -31,3 +31,5 @@ export default class StudentContainer extends Component {
     );
   }
 }
+
+StudentContainer.propTypes = { student: PropTypes.object };
--- src/components/views/StudentsView.jsx
+++ src/components/views/StudentsView.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import { NavbarContainer, StudentContainer } from "../containers";
 
 export default function StudentsView(props) {
+  let elems = [
+    {
+      id: 0,
+      name: "Xing Tao Shi",
+      campus: "Hunter College",
+      GPA: "3.95",
+      email: "xshi603@gmail.com",
+      image: "https://i.ya-webdesign.com/images/no-avatar-png-1.png"
+    },
+    {
+      id: 1,
+      name: "Khinshan Khan",
+      campus: "Hunter College",
+      GPA: "4.00",
+      email: "kkhan@gmail.com",
+      image: "https://i.ya-webdesign.com/images/no-avatar-png-1.png"
+    }
+  ];
+
+  let results = elems.map(element => <StudentContainer student={element} />);
   return (
     <div>
-      <NavbarContainer />
-      <br />
-      <StudentContainer />
-      <br />
-      <StudentContainer />
-      <br />
-      <StudentContainer />
+      <NavbarContainer /> <br />
+      {results}
     </div>
   );
 }
