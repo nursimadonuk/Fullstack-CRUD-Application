82ceccb6f46b4d0812e844062598c1c7b7eb9e6b
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-14 11:13:44 -0500

    defined more action constants

--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -1,3 +1,23 @@
+/* Adds an object to the store and to the database */
+
 export const ADD_CAMPUS = "ADD_CAMPUS";
 
 export const ADD_STUDENT = "ADD_STUDENT";
+
+/* Gets list of objects from database and fills the store with them */
+
+export const FETCH_STUDENTS = "FETCH_STUDENTS";
+
+export const FETCH_CAMPUSES = "FETCH_CAMPUSES";
+
+/* Deletes individual objects from the store, and from the database */
+
+export const DELETE_STUDENT = "DELETE_STUDENT";
+
+export const DELETE_CAMPUS = "DELETE_CAMPUS";
+
+/* Changes an individual object, given its ID */
+
+export const EDIT_STUDENT = "";
+
+export const EDIT_CAMPUS = "";
