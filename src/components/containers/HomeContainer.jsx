3c07b84c9aedb1ff6d8079b562a991dc7918a434
Khinshan Khan <khanshan501@gmail.com>
2020-01-16 00:03:05 -0500

    fixing component view relationships

--- src/components/containers/HomeContainer.jsx
+++ src/components/containers/HomeContainer.jsx
@@ -1,35 +1,9 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
-import "./HomeContainer.css";
-import logo from "./../../images/logo.png";
+import { HomeView } from "../views";
 
-export default class HomeComponent extends Component {
+export default class HomeContainer extends Component {
   render() {
-    return (
-      <div className="wrapper">
-        <Link to="/" className="logo">
-          <img src={logo} alt="yes" />
-        </Link>
-        <div id="description">
-          <p>
-            Full Stack Crud App <br />
-            Project Description Goes Here
-          </p>
-        </div>
-        <div>
-          <Link to="/students">
-            <button className="btn btn2" id="two">
-              View Students
-            </button>
-          </Link>
-          <Link to="/campuses">
-            <button className="btn btn1" id="one">
-              View Campuses
-            </button>
-          </Link>
-        </div>
-      </div>
-    );
+    return <HomeView />;
   }
 }
--- src/components/views/HomeView.jsx
+++ src/components/views/HomeView.jsx
@@ -1,6 +1,33 @@
 import React from "react";
-import { HomeContainer } from "../containers";
+import { Link } from "react-router-dom";
+
+import "./HomeView.css";
+import logo from "./../../images/logo.png";
 
 export default function HomeView(props) {
-  return <HomeContainer />;
+  return (
+    <div className="wrapper">
+      <Link to="/" className="logo">
+        <img src={logo} alt="yes" />
+      </Link>
+      <div id="description">
+        <p>
+          Full Stack Crud App <br />
+          Project Description Goes Here
+        </p>
+      </div>
+      <div>
+        <Link to="/students">
+          <button className="btn btn2" id="two">
+            View Students
+          </button>
+        </Link>
+        <Link to="/campuses">
+          <button className="btn btn1" id="one">
+            View Campuses
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
