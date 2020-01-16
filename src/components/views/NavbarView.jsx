cbee34e8ddab2cd75005af5a1951187c30c36d80
Khinshan Khan <khanshan501@gmail.com>
2020-01-15 22:06:37 -0500

    successfully passing in store to student view, need to refactor team misconception though

---
+++ src/components/views/NavbarView.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import "./NavbarView.css";
+import logo from "./../../images/logo.png";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          <img src={logo} alt="yes" />
+        </Link>
+        <ul className="nav-links">
+          <li className="nav-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/students">Students</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/campuses">Campuses</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -2,3 +2,5 @@ export { default as HomeView } from "./HomeView";
 export { default as StudentsView } from "./StudentsView";
 export { default as CampusesView } from "./CampusesView";
 export { default as NotFoundView } from "./404View";
+
+export { default as NavbarView } from "./NavbarView";
