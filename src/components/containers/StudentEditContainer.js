881c864fdddc26f3954c1d8f41daa9b4d8a69cca
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-16 16:40:00 -0500

    separated container and view for editing form; this commit doesnt work yet

--- src/components/containers/StudentEditContainer.js
+++ src/components/containers/StudentEditContainer.js
@@ -1,75 +1,38 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 
-import { NavbarView } from "../views";
+import { fetchAllStudentsThunk } from "../../thunks";
+import { NavbarView, StudentEditView } from "../views";
 
 import "./NavbarContainer.css";
 
-export default class StudentEditContainer extends Component {
+class StudentEditContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: parseInt(this.props.match.params.id)
+        }
+    } 
     render() {
         return (
             <div>
                 <NavbarView />
-                <div class = "form">
-                    <form>
-                        <tr>
-                            <td>
-                                First Name:
-                            </td>
-                            <td>
-                                <input type="text"></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Last Name:
-                            </td>
-                            <td>
-                                <input type="text"></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Campus:
-                            </td>
-                            <td>
-                                <input type="text"></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                GPA:
-                            </td>
-                            <td>
-                                <input type="text"></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Email:
-                            </td>
-                            <td>
-                                <input type="text"></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                Image:
-                            </td>
-                            <td>
-                                <input type="text"></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <br/>
-                        </tr>
-                        <tr>
-                            <button className="btn btn1" id="one">Submit</button>
-                        </tr>
-                    </form>
-                </div>
+                <StudentEditView student={"hi"} />
             </div>
         )
     }
 }
 
+function mapState(state) {
+  return {
+    allStudents: state.rootReducer
+  };
+}
+
+function mapDispatch(dispatch) {
+  return {
+    fetchAllStudents: () => dispatch(fetchAllStudentsThunk())
+  };
+}
+
+export default connect(mapState, mapDispatch)(StudentEditContainer);
--- src/components/views/index.js
+++ src/components/views/index.js
@@ -2,5 +2,6 @@ export { default as HomeView } from "./HomeView";
 export { default as StudentsView } from "./StudentsView";
 export { default as CampusesView } from "./CampusesView";
 export { default as NotFoundView } from "./404View";
+export { default as StudentEditView } from "./StudentEditView";
 
 export { default as NavbarView } from "./NavbarView";
