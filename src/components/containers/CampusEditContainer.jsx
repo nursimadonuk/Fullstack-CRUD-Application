9f6496cd6255548606be35d3a870e701b808b840
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-17 03:53:23 -0500

    changed fields to match campus

--- src/components/containers/CampusEditContainer.jsx
+++ src/components/containers/CampusEditContainer.jsx
@@ -1,41 +1,38 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import { fetchAllCampusesThunk, fetchAllCampusesThunk, editCampusThunk, addCampusThunk } from "../../thunks";
+import { fetchAllCampusesThunk, editCampusThunk, addCampusThunk } from "../../thunks";
 import { NavbarView, CampusEditView } from "../views";
 
 class CampusEditContainer extends Component {
   constructor(props) {
     super(props);
     console.log(props);
-    this.modes = {EDIT: "Editing Student", NEW: "Enter information for the new student", INVALID: "This student was not found"}
+    this.modes = {EDIT: "Editing Campus", NEW: "Enter information for the new campus", INVALID: "This campus was not found"}
     this.state = {
-      firstName: "",
-      lastName: "",
-      campus: "",
-      GPA: "",
-      email: "",
+      name: "",
+      address: "",
+      description: "",
       image: "",
       id: null,
       /* A boolean that indicates whether we have initialized this 
-      component with values from the current student's values yet. 
-      Will be set to true if the student is found in the database 
+      component with values from the current campus's values yet. 
+      Will be set to true if the campus is found in the database 
       and the redux store is updated. Used to prevent an infinite 
       loop in componentDidUpdate */
       initialized: false,
       mode: ""
     };
-    this.props.fetchAllCampuses();
     /* Tests whether the url parameter (not query string) "id" is a
     number or not. Uses built-in javascript isNaN() function. If no
     "id" was provided, it will be defined, and therefore not a number.
-    In this case, we can assume that you are adding a new student. */
+    In this case, we can assume that you are adding a new campus. */
     if(this.props.match.params.id == undefined) {
       this.state.mode = this.modes.NEW;
     }
     else if(!isNaN(this.props.match.params.id)) {
       this.state.id = parseInt(this.props.match.params.id);
-      this.props.fetchAllStudents();
+      this.props.fetchAllCampuses();
       this.state.mode = this.modes.EDIT;
     }
     else {
@@ -44,13 +41,13 @@ class CampusEditContainer extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if(prevState.initialized == false && this.props.students.length > 0) {
-      let student = this.props.students.filter(
-        student => student.id === this.state.id
+    if(prevState.initialized == false && this.props.campuses.length > 0) {
+      let campusById = this.props.campuses.filter(
+        campus => campus.id === this.state.id
       );
-      if(student.length != 0) {
+      if(campusById.length != 0) {
         this.setState({
-          ...student[0], 
+          ...campusById[0], 
           initialized: true
         });
       }
@@ -63,20 +60,14 @@ class CampusEditContainer extends Component {
     }
   }
 
-  handleFirstName = e => {
-    this.setState({ firstName: e.target.value });
+  handleName = e => {
+    this.setState({ name: e.target.value });
   };
-  handleLastName = e => {
-    this.setState({ lastName: e.target.value });
+  handleAddress = e => {
+    this.setState({ address: e.target.value });
   };
-  handleCampus = e => {
-    this.setState({ campus: e.target.value });
-  };
-  handleGPA = e => {
-    this.setState({ GPA: e.target.value });
-  };
-  handleEmail = e => {
-    this.setState({ email: e.target.value });
+  handleDescription = e => {
+    this.setState({ description: e.target.value });
   };
   handleImage = e => {
     this.setState({ image: e.target.value });
@@ -84,11 +75,10 @@ class CampusEditContainer extends Component {
   handleSubmission = e => {
     e.stopPropagation();
     e.nativeEvent.stopImmediatePropagation();
-    console.log(this.state.id);
     if(this.state.id != null) {
-      this.props.editStudent(this.state);
+      this.props.editCampus(this.state);
     } else {
-      this.props.addStudent(this.state);
+      this.props.addCampus(this.state);
     }
   }
 
@@ -99,14 +89,12 @@ class CampusEditContainer extends Component {
         <NavbarView />
         <h1>{this.state.mode}</h1>
         {(this.state.mode != this.modes.INVALID) && (
-        <StudentEditView 
-            student={this.state} 
+        <CampusEditView 
+            prevValues={this.state} 
             campuses={this.props.campuses}
-            handleFirstName={this.handleFirstName}
-            handleLastName={this.handleLastName}
-            handleCampus={this.handleCampus}
-            handleGPA={this.handleGPA}
-            handleEmail={this.handleEmail}
+            handleName={this.handleName}
+            handleAddress={this.handleAddress}
+            handleDescription={this.handleDescription}
             handleImage={this.handleImage}
             handleSubmission={this.handleSubmission}
         />
@@ -118,17 +106,15 @@ class CampusEditContainer extends Component {
 
 function mapState(state) {
   return {
-    students: state.rootReducer.STUDENT_LIST,
     campuses: state.rootReducer.CAMPUS_LIST
   };
 }
 
 function mapDispatch(dispatch) {
   return {
-    fetchAllStudents: () => dispatch(fetchAllStudentsThunk()),
     fetchAllCampuses: () => dispatch(fetchAllCampusesThunk()),
-    editStudent: (changes) => dispatch(editStudentThunk(changes)),
-    addStudent: (student) => dispatch(addStudentThunk(student))
+    editCampus: (changes) => dispatch(editCampusThunk(changes)),
+    addCampus: (newCampus) => dispatch(addCampusThunk(newCampus))
   };
 }
 
