864fc6490291a175d9ed56d1a390929aaed3cfb4
Xing Tao Shi <xshi603@gmail.com>
2020-01-16 22:18:32 -0500

    handlers

--- src/components/containers/StudentEditContainer.js
+++ src/components/containers/StudentEditContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { fetchAllStudentsThunk } from "../../thunks";
@@ -7,20 +7,44 @@ import { NavbarView, StudentEditView } from "../views";
 import "./NavbarContainer.css";
 
 class StudentEditContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: parseInt(this.props.match.params.id)
-        }
-    } 
-    render() {
-        return (
-            <div>
-                <NavbarView />
-                <StudentEditView student={"hi"} />
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: parseInt(this.props.match.params.id),
+      firstName: "",
+      lastName: "",
+      campus: "",
+      GPA: "",
+      email: "",
+      image: ""
+    };
+  }
+  handleFirstName = e => {
+    this.setState({ firstName: e.target.value });
+  };
+  handleLastName = e => {
+    this.setState({ lastName: e.target.value });
+  };
+  handleCampus = e => {
+    this.setState({ campus: e.target.value });
+  };
+  handleGPA = e => {
+    this.setState({ GPA: e.target.value });
+  };
+  handleEmail = e => {
+    this.setState({ email: e.target.value });
+  };
+  handleImage = e => {
+    this.setState({ image: e.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <NavbarView />
+        <StudentEditView student={"hi"} />
+      </div>
+    );
+  }
 }
 
 function mapState(state) {
--- src/components/views/StudentEditView.jsx
+++ src/components/views/StudentEditView.jsx
@@ -2,65 +2,63 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function StudentEditView(props) {
-    return (
-        <div>
-            <table className = "form">
-                <tbody>
-                    <tr>
-                        <td>
-                            First Name:
-                        </td>
-                        <td>
-                            <input type="text" onChange={props.handleFirstName} value={props.student.firstName}></input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Last Name:
-                        </td>
-                        <td>
-                            <input type="text" onChange={props.handleLastName} value={props.student.lastName}><input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Campus:
-                        </td>
-                        <td>
-                            <select onChange={props.handleCampus}>
-                                {props.campuses.map(campus =>
-                                    <option value={campus.id}>{campus.name}</option>
-                                )}
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            GPA:
-                        </td>
-                        <td>
-                            <input type="number" onChange={props.handleGPA}></input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Email:
-                        </td>
-                        <td>
-                            <input type="text" onChange={props.handleEmail}></input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Image:
-                        </td>
-                        <td>
-                            <input type="text" onChange={props.handleImage}></input>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <button className="btn btn1" id="one" onClick={props.handleSubmission}>Submit</button>
-        </div>
-    );
+  return (
+    <div>
+      <table className="form">
+        <tbody>
+          <tr>
+            <td>First Name:</td>
+            <td>
+              <input
+                type="text"
+                onChange={props.handleFirstName}
+                value={props.student.firstName}
+              ></input>
+            </td>
+          </tr>
+          <tr>
+            <td>Last Name:</td>
+            <td>
+              <input
+                type="text"
+                onChange={props.handleLastName}
+                value={props.student.lastName}
+              ></input>
+            </td>
+          </tr>
+          <tr>
+            <td>Campus:</td>
+            <td>
+              <select onChange={props.handleCampus}>
+                {props.campuses.map(campus => (
+                  <option value={campus.id}>{campus.name}</option>
+                ))}
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>GPA:</td>
+            <td>
+              <input type="number" onChange={props.handleGPA}></input>
+            </td>
+          </tr>
+          <tr>
+            <td>Email:</td>
+            <td>
+              <input type="text" onChange={props.handleEmail}></input>
+            </td>
+          </tr>
+          <tr>
+            <td>Image:</td>
+            <td>
+              <input type="text" onChange={props.handleImage}></input>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <button className="btn btn1" id="one" onClick={props.handleSubmission}>
+        Submit
+      </button>
+    </div>
+  );
 }
